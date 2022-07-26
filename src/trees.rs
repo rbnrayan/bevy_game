b108ea6e36f6f2c6e2b66d676721b71e307941c6
rbnrayan <rayan.robion7@gmail.com>
2022-07-26 13:08:25 +0200

    refactoring + tree respawn each 60secs

--- src/trees.rs
+++ src/trees.rs
@@ -16,7 +16,8 @@ pub struct TreePlugin;
 
 impl Plugin for TreePlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_tree.after(spawn_map))
+        app.add_startup_system(spawn_trees.after(spawn_map))
+            .add_system(check_tree_amount)
             .add_system(animate_tree);
     }
 }
@@ -26,13 +27,15 @@ pub struct Tree {
     pub health: i16,
 }
 
-pub fn spawn_tree(
+#[derive(Component)]
+pub struct TreeTimer(pub Timer);
+
+pub fn spawn_trees(
     mut commands: Commands,
     mut map: ResMut<Map>,
     texture_atlas_handle: Res<AtlasHandle>,
     tree_query: Query<&Transform, With<Tree>>,
 ) {
-    let texture_sprite = TextureAtlasSprite::new(15);
     let mut tree_amount = tree_query.iter().count();
     let mut rng = rand::thread_rng();
 
@@ -51,31 +54,55 @@ pub fn spawn_tree(
         }
 
         map.push(
-            commands
-                .spawn_bundle(SpriteSheetBundle {
-                    sprite: texture_sprite.clone(),
-                    texture_atlas: texture_atlas_handle.clone(),
-                    transform: Transform::from_scale(Vec3::splat(SCALE)).with_translation(
-                        Vec3::new(
-                            x as f32 * SCALE * TILE_SIZE,
-                            y as f32 * SCALE * TILE_SIZE,
-                            20.0,
-                        ),
-                    ),
-                    ..Default::default()
-                })
-                .insert(Tree { health: 100 })
-                .insert(Animations {
-                    animations: vec![Animation {
-                        frames: vec![15, 16, 17, 18, 19],
-                        current_frame: 0,
-                        timer: AnimationTimer(Timer::from_seconds(0.5, true)),
-                    }],
-                })
-                .id(),
+            match spawn_tree(&mut commands, &texture_atlas_handle, &tree_query) {
+                Some(e) => e,
+                None => continue,
+            }
         );
         tree_amount += 1;
     }
+    commands.spawn().insert(TreeTimer(Timer::from_seconds(60.0, true)));
+}
+
+fn spawn_tree(
+    commands: &mut Commands,
+    texture_atlas_handle: &Res<AtlasHandle>,
+    tree_query: &Query<&Transform, With<Tree>>,
+) -> Option<Entity> {
+    let texture_sprite = TextureAtlasSprite::new(15);
+    let mut rng = rand::thread_rng();
+
+    let (x, y) = (
+        rng.gen_range(-(TILE_COUNT_X as i32)..=TILE_COUNT_X as i32),
+        rng.gen_range(-(TILE_COUNT_Y as i32)..=TILE_COUNT_Y as i32),
+    );
+
+    if check_tree_position(Vec2::new(x as f32, y as f32), &tree_query) {
+        return None;
+    }
+
+    Some(commands
+        .spawn_bundle(SpriteSheetBundle {
+            sprite: texture_sprite.clone(),
+            texture_atlas: (*texture_atlas_handle).clone(),
+            transform: Transform::from_scale(Vec3::splat(SCALE)).with_translation(
+                Vec3::new(
+                    x as f32 * SCALE * TILE_SIZE,
+                    y as f32 * SCALE * TILE_SIZE,
+                    20.0,
+                ),
+            ),
+            ..Default::default()
+        })
+        .insert(Tree { health: 100 })
+        .insert(Animations {
+            animations: vec![Animation {
+                frames: vec![15, 16, 17, 18, 19],
+                current_frame: 0,
+                timer: AnimationTimer(Timer::from_seconds(0.5, true)),
+            }],
+        })
+        .id())
 }
 
 pub fn check_tree_position(pos: Vec2, tree_query: &Query<&Transform, With<Tree>>) -> bool {
@@ -93,5 +120,24 @@ fn animate_tree(
 ) {
     for (mut sprite, mut animations) in tree_query.iter_mut() {
         animations.animations[0].update(&time, &mut sprite);
+    }
+}
+
+fn check_tree_amount(
+    time: Res<Time>,
+    texture_atlas_handle: Res<AtlasHandle>,
+    mut map: ResMut<Map>,
+    mut commands: Commands,
+    mut timer_query: Query<&mut TreeTimer>,
+    tree_query: Query<&Transform, With<Tree>>,
+) {
+    let mut tree_timer = timer_query.single_mut();
+
+    tree_timer.0.tick(time.delta());
+
+    if tree_timer.0.finished() && tree_query.iter().count() < TREE_AMOUNT {
+        if let Some(e) = spawn_tree(&mut commands, &texture_atlas_handle, &tree_query) {
+            map.push(e);
+        }
     }
 }
